e208be7ea40a6a7f59bdafd53139f301f45ee8d2
septianmalela <maulanas430@gmail.com>
2022-06-05 07:36:43 +0530

    Add device

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+         sessions: 'users/sessions',
+    registrations: 'users/registrations',
+      invitations: 'users/invitations',
+        passwords: 'users/passwords'
+  }
+  resources :homes
+
+  root 'homes#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
